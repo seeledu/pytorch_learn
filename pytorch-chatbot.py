04201e987ada2eed4d803e2014e58d165d23aef9
seeledu <531609175@qq.com>
2018-10-10 23:34:20 +0800

    add download

--- pytorch-chatbot.py
+++ pytorch-chatbot.py
@@ -12,5 +12,32 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 import torch
-from torch.jit import trace
+from torch.jit import trace,script
 import torch.nn as nn
+from torch import optim
+import torch.nn.functional as F
+import csv
+import random
+import re
+import os
+import unicodedata
+import codecs
+from io import open
+import itertools
+import math
+
+# 检验能否用gpu,不行的话设定为cpu模式.
+USE_CUDA = torch.cuda.is_available()
+# print(USE_CUDA)
+device = torch.device("cuda" if USE_CUDA else "cpu")
+
+# 下载语料
+corpus_name = "cornell movie-dialogs corpus"
+corpus = os.path.join("data", corpus_name)
+def printLiunes(file, n= 10):
+    with open(file, 'rb') as datafile:
+        lines = datafile.readlines()
+    for line in lines[:n]:
+        print(line)
+
+printLiunes(os.path.join(corpus,"movie_lines.txt"))
